9a2d1ac40907c76b43c3cd9bc1d35531d406e032
FilipTota <58394415+FilipTota@users.noreply.github.com>
2020-02-20 09:43:55 +0100

    Updates

---
+++ precache-manifest.2c6e68d8c92a3db5f08364ae934f7273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ab9dd7ac51f73095aaed8a8031c0bf",
+    "url": "/stopwatch/index.html"
+  },
+  {
+    "revision": "77e5570696f44250009c",
+    "url": "/stopwatch/static/css/main.c8975bc5.chunk.css"
+  },
+  {
+    "revision": "f8a498ff671fdaff3cc1",
+    "url": "/stopwatch/static/js/2.fdf34742.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/stopwatch/static/js/2.fdf34742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e5570696f44250009c",
+    "url": "/stopwatch/static/js/main.6baa9039.chunk.js"
+  },
+  {
+    "revision": "28d0cddc29cae312355f",
+    "url": "/stopwatch/static/js/runtime-main.1202d4b8.js"
+  }
+]);
